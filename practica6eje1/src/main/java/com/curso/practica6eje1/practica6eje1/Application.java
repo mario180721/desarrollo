fe1b4cd1d6eaf7b0c04643e21cccf23310696f45
lucia8716 <63652719+lucia8716@users.noreply.github.com>
2021-08-26 15:47:49 +0200

    v1

--- practica6eje1/src/main/java/com/curso/practica6eje1/practica6eje1/Application.java
+++ practica6eje1/src/main/java/com/curso/practica6eje1/practica6eje1/Application.java
@@ -32,11 +32,7 @@ public class Application
 		System.out.println("Creating her Savings Account with a 500.00 balance and 500.");
 		customer.setAccount(new CheckingAccount(500.00,500.00));
 		
-		System.out.println(" Creando el cliente Maria Soley");
-		banco.addCustomer("Maria", "Soley");
-		customer = banco.getCustomer(3);
-		System.out.println("Maria shares her Checking Account with her husband Tim.");
-		customer.setAccount(banco.getCustomer(2).getAccount());
+		
 		
 		 System.out.println();
 		 
@@ -56,6 +52,20 @@ public class Application
 		    
 		    System.out.println("Retrieving the customer Jane Smith with her savings account.");
 		    customer = banco.getCustomer(1);
+		    account = customer.getAccount();
+		    
+		    System.out.println("Withdraw 150.00: " + account.withdraw(150.00));
+		    System.out.println("Deposit 22.50: " + account.deposit(22.50));
+		    System.out.println("Withdraw 47.62: " + account.withdraw(47.62));
+		    System.out.println("Withdraw 400.00: " + account.withdraw(400.00));
+		    System.out.println("Customer [" + customer.getLastName()
+				       + ", " + customer.getFirstName()
+				       + "] has a balance of " + account.getBalance());
+
+		    System.out.println();
+		    
+		    System.out.println("Retrieving the customer Tim Soley with her savings account that has overdraft protection..");
+		    customer = banco.getCustomer(2);
 		    account = customer.getAccount();
 		    
 		    System.out.println("Withdraw 150.00: " + account.withdraw(150.00));
